4ac521f7469abbe59d0a7e18ab598c87b34b3058
Piumini Abeysinghe <piuminiabeysinghe2003@gmail.com>
2026-08-04 11:06:42 +0530

    Add Maven wrapper files, project structure, and initial Spring Boot application setup

---
+++ src/main/java/com/example/ParcelGO/ParcelGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParcelGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParcelGO/ParcelGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParcelGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
